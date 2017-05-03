27810a0b4911ad32b4765ab3b94b4056d2c16b24
nashrzt <nishan.pa@razorthink.net>
2017-05-03 15:03:51 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
-rm -rf /ebs/zt_cassandramodified_master-POS6VGXMPC2VCQUFLZ4BMYZB3OGUVTASPJ6SSJG4XLKFNYOQRIUQ/.git
+rm -rf /ebs/zt_cassandramodified_master-POS6VGXMPC2VCQUFLZ4BMYZB3OGUVTASPJ6SSJG4XLKFNYOQRIUQ
 sudo rm -rf /var/lib/jenkins/workspace/zt_cassandramodified_master-POS6VGXMPC2VCQUFLZ4BMYZB3OGUVTASPJ6SSJG4XLKFNYOQRIUQ/.git 
 sudo cp -rf /var/lib/jenkins/workspace/zt_cassandramodified_master-POS6VGXMPC2VCQUFLZ4BMYZB3OGUVTASPJ6SSJG4XLKFNYOQRIUQ /ebs/
 
