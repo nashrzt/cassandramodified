38a55679273d8554476cd4e9729a21640c6bc21a
nashrzt <nishan.pa@razorthink.net>
2017-05-03 14:36:26 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,1 +1,1 @@
-cp -rf /var/lib/jenkins/workspace/zt_cassandramodified_master-POS6VGXMPC2VCQUFLZ4BMYZB3OGUVTASPJ6SSJG4XLKFNYOQRIUQ /ebs/
+sudo cp -rf /var/lib/jenkins/workspace/zt_cassandramodified_master-POS6VGXMPC2VCQUFLZ4BMYZB3OGUVTASPJ6SSJG4XLKFNYOQRIUQ /ebs/
